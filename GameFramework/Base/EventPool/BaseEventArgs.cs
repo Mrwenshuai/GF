85ffda0d250ad2583005261b418691feb2995df8
Mrwenshuai <1154355867@qq.com>
2019-07-16 10:44:17 +0800

    module of resources and downloader

---
+++ GameFramework/Base/EventPool/BaseEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace GameFramework
+{
+    /// <summary>
+    /// 事件基类。
+    /// </summary>
+    public abstract class BaseEventArgs : GameFrameworkEventArgs, IReference
+    {
+        /// <summary>
+        /// 获取类型编号。
+        /// </summary>
+        public abstract int Id { get; }
+
+        /// <summary>
+        /// 清理引用。
+        /// </summary>
+        public abstract void Clear();
+    }
+}
---
+++ GameFramework/Resource/ResourceManager.ResourceLoader.LoadResourceAgent.WaitingType.cs
@@ -0,0 +1,19 @@
+﻿namespace GameFramework.Resource
+{
+    internal sealed partial class ResourceManager : GameFrameworkModule, IResourceManager
+    {
+        private sealed partial class ResourceLoader
+        {
+            private sealed partial class LoadResourceAgent : ITaskAgent<LoadResourceTaskBase>
+            {
+                private enum WaitingType
+                {
+                    None = 0,
+                    WaitForAsset,
+                    WaitForDependencyAsset,
+                    WaitForResource,
+                }
+            }
+        }
+    }
+}
